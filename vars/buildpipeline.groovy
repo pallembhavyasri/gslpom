b2fa84b6774c6a8c613429e7c0d10884f0c9a24c
Bhavya Sri P <165980888+pallembhavyasri@users.noreply.github.com>
2025-11-07 21:27:49 +0530

    Update buildpipeline.groovy

--- vars/buildpipeline.groovy
+++ vars/buildpipeline.groovy
@@ -1,6 +1,9 @@
 
 def call(String repoUrl, String branch = 'main') {
+    echo "Hello"
     def obj = new com.example.test()
+    echo "HI"
     //obj.cloneRepo(repoUrl, branch)
     obj.test1(repoUrl, branch)
+    echo "heyy"
 }
