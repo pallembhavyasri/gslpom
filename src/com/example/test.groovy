0a4c8c3ac229c3987ff234ccd90329c3326433db
Bhavya Sri P <165980888+pallembhavyasri@users.noreply.github.com>
2025-11-07 21:30:16 +0530

    Update test.groovy

--- src/com/example/test.groovy
+++ src/com/example/test.groovy
@@ -1,9 +1,14 @@
 package com.example
 
     void test1(String repoUrl, String branch = 'main') {
+        echo "11"
+        node("master"){
+        echo "22"
         def mvnHome = tool name: 'maven', type: 'maven'
         stage('Clone & Build') {
+            echo "33"
             dir('repo') {
+                echo "44"
                 git url: repoUrl, branch: branch
                 def output = sh(script: "grep -r '.pom' .", returnStatus: true)
                 if (output == 0) {
@@ -13,4 +18,5 @@ package com.example
                 }
             }
         }
+    }
     }
