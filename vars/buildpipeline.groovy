64ad3cb035c1ff2ea8ac3de7af8a936744b95574
Bhavya Sri Pallem <palbhavy@sutherlandglobal.com>
2025-11-07 21:23:55 +0530

    vars

--- vars/buildpipeline.groovy
+++ vars/buildpipeline.groovy
@@ -1,5 +1,6 @@
 
 def call(String repoUrl, String branch = 'main') {
-    def obj = new com.example.utility(this)
-    obj.cloneRepo(repoUrl, branch)
+    def obj = new com.example.test()
+    //obj.cloneRepo(repoUrl, branch)
+    obj.test1(repoUrl, branch)
 }
