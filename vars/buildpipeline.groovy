f0370b8483601954ee4eb585d8a2e65fa12f2838
Bhavya Sri Pallem <palbhavy@sutherlandglobal.com>
2025-11-09 11:08:05 +0530

    vars

--- vars/buildpipeline.groovy
+++ vars/buildpipeline.groovy
@@ -1,10 +1,7 @@
 
 def call() {
     if(JOB_NAME.contains("3-repos")){
-    //def obj = new com.example.test()
     def obj1 = new com.example.replay()
-    //obj.cloneRepo(repoUrl, branch)
-    //obj.test1(repoUrl, branch)
     obj1.reposClone()
 }
     else if(JOB_NAME.contains("test")){
